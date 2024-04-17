24ce2ae1b3f36a365181e0d6a8cb05a1159cbaf9
MuhammadAliffandy <aliffandyrealgo@gmail.com>
2024-04-17 14:53:02 +0700

    fix: fixed env base url

--- app/utils/constant.js
+++ app/utils/constant.js
@@ -1,2 +1,2 @@
-export const BASE_URL= process.env.NODE_ENV === 'development' ? process.env.BASE_URL_DEV || '' : process.env.BASE_URL || '';
+export const BASE_URL= process.env.NODE_ENV === 'development' ? process.env.BASE_URL_DEV || '' : process.env.BACKEND_URL || '';
 export const PAGINATION_BATCH = 10
