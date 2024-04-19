66f799a86b000fb47e506a2ddd7e6fc40762b0c4
MuhammadAliffandy <aliffandyrealgo@gmail.com>
2024-04-19 14:58:53 +0700

    feat: testing config

--- app/utils/constant.js
+++ app/utils/constant.js
@@ -1,2 +1,4 @@
 export const BASE_URL= process.env.NODE_ENV === 'development' ? process.env.BASE_URL_DEV || '' : process.env.BACKEND_URL || '';
+console.log('BASE URL')
+console.log(BASE URL)
 export const PAGINATION_BATCH = 10
