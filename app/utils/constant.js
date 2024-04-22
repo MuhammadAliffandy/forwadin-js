68875676bf3ecb4fc15d5c3fd063de62e0631873
MuhammadAliffandy <aliffandyrealgo@gmail.com>
2024-04-22 11:14:37 +0700

    fix : fixed bug at some many page

--- app/utils/constant.js
+++ app/utils/constant.js
@@ -1,2 +1,4 @@
 export const BASE_URL= process.env.NODE_ENV === 'development' ? process.env.BASE_URL_DEV : process.env.BASE_URL ;
+console.log(process.env.NODE_ENV)
+console.log(BASE_URL)
 export const PAGINATION_BATCH = 10
